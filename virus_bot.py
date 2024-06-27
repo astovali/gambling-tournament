58b649761c0b5aae9ab29d9bd21f8dc0f7ae8f6d
Oscar Green <oscar.green@student.education.wa.edu.au>
2024-06-27 18:08:10 +0800

    Unpredictablitity added

--- virus_bot.py
+++ virus_bot.py
@@ -1,5 +1,6 @@
 from dealer import Dealer, Card
 from math import ceil
+import random as rd
 
 class VirusBot:
     '''
@@ -19,6 +20,7 @@ class VirusBot:
     def move(self, data):
         match_value = max([x["bet"] for x in data["others"]] + [data["self"]["bet"]])
         bet = match_value
+        pre = int(rd.randint(0,10))
         if len(data["pool"]) == 0:
             if data["self"]["hand"][0].value == data["self"]["hand"][1].value:
                 if data["self"]["hand"][0].value == 14 and data["self"]["hand"][1].value == 14:
@@ -27,31 +29,60 @@ class VirusBot:
                     bet = match_value*2
             elif data["self"]["hand"][0].value != data["self"]["hand"][1].value:
                 if data["self"]["hand"][0].value == 14 or data["self"]["hand"][1].value == 14:
-                    bet = match_value*2
+                    if pre <= 3:
+                        bet = match_value*2
+                    elif pre > 3:
+                        bet = match_value
                 elif data["self"]["hand"][0].value > 10 and data["self"]["hand"][1].value > 10:
-                    bet = match_value*1.5
+                    if pre <= 3:
+                        bet = match_value*1.5
+                    elif pre > 3:
+                        bet = match_value
         else:
             hand = data["self"]["hand"] + data["pool"]
             value = Dealer.evaluate_hand(hand)
             evaluate_value = max([card.value for card in hand])
             ranking = (value - evaluate_value)/14
+            unpre = int(rd.randint(0,10))
             if ranking == 8:
-                bet = data["self"]["money"]
+                if unpre <= 3:
+                    bet = data["self"]["money"]
+                elif unpre > 3:
+                    bet = match_value
             elif ranking == 7:
-                bet = match_value*10
+                if unpre <= 3:
+                    bet = match_value*7.5
+                elif unpre > 3:
+                    bet = match_value
             elif ranking == 0:
-                bet = match_value*10
+                if unpre <= 2:
+                    bet = match_value*7.5
+                elif unpre > 3:
+                    bet = match_value
             elif ranking == 1 or ranking == 2:
-                bet = match_value
+                unpred = int(rd.randint(0,1000))
+                if unpred == 0:
+                    return 'F'
+                else:
+                    bet = match_value
             elif ranking == 6:
-                bet = match_value*5
+                if unpre <= 3:
+                    bet = match_value*5
+                elif unpre > 3:
+                    bet = match_value
             elif ranking == 5 or ranking == 4:
-                bet = match_value*2.5
+                if unpre <= 3:
+                    bet = match_value*2.5
+                elif unpre > 3:
+                    bet = match_value
             elif ranking == 3:
-                bet = match_value*1.5
+                if unpre <= 3:
+                    bet = match_value*1.5
+                elif unpre > 3:
+                    bet = match_value
         if bet < match_value:
             return min(match_value, data["self"]["money"])
-        if data["self"]["money"] < bet:
+        elif data["self"]["money"] < bet:
             return data["self"]["money"]
         if data["self"]["money"] < match_value:
             return 'F'
@@ -59,4 +90,3 @@ class VirusBot:
     
     def cleanup(self, data):
         pass
-    
